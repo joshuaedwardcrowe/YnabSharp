c8b8269704d357c4122cefc9f5770e4d398b82c0
Joshua Crowe <joshuaedwardcrowe@icloud.com>
2026-02-03 18:04:18 +0000

    Added GitIgnore

---
+++ YnabSharp.Seeder/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using KitCli;
+using YnabSharp.Seeder;
+
+var app = new CliAppBuilder()
+    .WithCli<YnabSharpSeederCliApp>()
+    .WithUserSecretSettings()
+    .WithSettings<YnabSharpSeederSettings>()
+    .WithRegistry<YnabRegistry>()
+    .WithRegistry<YnabSharpSeederRegistry>();
+
+await app.Run();
